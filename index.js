771f35541d5a4c20236c54933161bb35c52df0d1
AbhayPS <abhaypratap3062002@gmail.com>
2023-10-14 11:15:50 +0530

    initial commints

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+const appSettings = {
+    databaseURL: "https://axial-edition-371811-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementListInDB = ref(database,"endorsementList")
+
+
+
+const endorsementInputFieldEl = document.getElementById("endorsement-field")
+const fromInputFieldEl = document.getElementById("from-el")
+const toInputFieldEl = document.getElementById("to-el")
+const btnEl = document.getElementById("btn")
+const endorsementListEl = document.getElementById("endorsement-list")
+ 
+
+
+btnEl.addEventListener("click",function(){
+   let inputValue=endorsementInputFieldEl.value
+   push( endorsementListInDB, inputValue)
+   endorsementListEl.innerHTML+=`<li>${inputValue}</li>`
+})
+
+endorsementListEl.addEventListener("click", function(){
+    clearField()
+})
+function clearField(){
+    endorsementListEl.innerHTML=""
+}
